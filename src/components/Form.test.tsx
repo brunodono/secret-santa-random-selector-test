332747f6385e31ced9f2a3bcc6df438b6198c496
brunodono <bruno_bignard@hotmail.com>
2022-09-06 15:49:51 +0100

    Created a new test to form.test

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,21 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Form from "./Form";
 
 
-test('When the input is empty, new participants can not be added',() => {
+test('when the input is empty, new participants can not be added',() => {
 
     render(<Form />)
-
-
     //find the input on DOM
     const input = screen.getByPlaceholderText("Enter the participants's names");
     // find the button
     const button = screen.getByRole('button');
-
     //make sure input is in the doc
     expect(input).toBeInTheDocument();
     //make sure the button is disabled
     expect(button).toBeDisabled();
+});
 
+test('add a new participant if there is a full filled name', () => {
+
+    render(<Form />)
+    //find the input on DOM
+    const input = screen.getByPlaceholderText("Enter the participants's names");
+    // find the button
+    const button = screen.getByRole('button');
+    //enter a value to input
+    fireEvent.change(input, {
+        target: {
+            value: 'Bruno Bignardi'
+        }
+    });
+
+    //click on submit button
+    fireEvent.click(button);
+
+    //make sure the input has active focus
+    expect(input).toHaveFocus();
+
+    //make sure the input has no value
+    expect(input).toHaveValue("");
+    
 })
